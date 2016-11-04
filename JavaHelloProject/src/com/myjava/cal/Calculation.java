537e9ff4633d9b028c23c01b02a6ad027f945249
lingaiah-ctu <rao.lingaiah@gmail.com>
2016-11-04 00:06:02 -0500

    added commit

--- JavaHelloProject/src/com/myjava/cal/Calculation.java
+++ JavaHelloProject/src/com/myjava/cal/Calculation.java
@@ -7,6 +7,10 @@ public class Calculation {
 	  int a=100,b=200,c;
 	  c=a+b;
 	  System.out.println("Addition of a and b : "+c);
+	  c=a*b;
+	  System.out.println("multiplication of a and b : "+c);
+	  c=b/a;
+	  System.out.println("Division of b and a : "+c);
 	  
 		// TODO Auto-generated method stub
 
