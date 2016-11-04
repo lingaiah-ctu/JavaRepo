5ff80cc812c4b318c230d542dac71fa256961be1
lingaiah-ctu <rao.lingaiah@gmail.com>
2016-11-03 23:56:09 -0500

    first commit

---
+++ JavaHelloProject/src/com/myjava/cal/Calculation.java
@@ -0,0 +1,15 @@
+package com.myjava.cal;
+
+public class Calculation {
+
+	public static void main(String[] args)
+	{
+	  int a=100,b=200,c;
+	  c=a+b;
+	  System.out.println("Addition of a and b : "+c);
+	  
+		// TODO Auto-generated method stub
+
+	}
+
+}
